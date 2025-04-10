264be7b2422d0032869c7f1eb0efe9887d178172
dj-oyu <68707227+dj-oyu@users.noreply.github.com>
2025-04-11 01:06:57 +0900

    feat: putsや末尾の複合式に含まれる変数もヒント除外。ヒントの露骨さを緩和。テスト＆ドキュメント更新

--- lib/code_parser.rb
+++ lib/code_parser.rb
@@ -22,15 +22,23 @@ class CodeParser
 
     # 末尾の変数評価を検出
     symbols[:tail_vars] = []
+    symbols[:tail_vars_inner] = []
     if sexp.is_a?(Array) && sexp[0] == :program && sexp[1].is_a?(Array)
       last_stmt = sexp[1].last
+      # 末尾が単一変数
       if last_stmt.is_a?(Array) && last_stmt[0] == :var_ref && last_stmt[1][0] == :@ident
         symbols[:tail_vars] << last_stmt[1][1]
       end
+      # 末尾が式なら、その中の変数も抽出
+      extract_inner_vars(last_stmt, symbols[:tail_vars_inner])
     end
 
+    # putsの引数に含まれる変数の中身も抽出
+    symbols[:puts_inner_vars] = []
+    CodeParser.extract_puts_inner_vars(sexp, symbols[:puts_inner_vars])
+
     # putsの引数と末尾の変数を除外
-    filtered_vars = symbols[:variables].uniq - symbols[:puts_args].uniq - symbols[:tail_vars].uniq
+    filtered_vars = symbols[:variables].uniq - symbols[:puts_args].uniq - symbols[:tail_vars].uniq - symbols[:tail_vars_inner].uniq - symbols[:puts_inner_vars].uniq
 
     # 重複を削除して返す
     {
@@ -38,8 +46,33 @@ class CodeParser
       methods: symbols[:methods].uniq,
       classes: symbols[:classes].uniq,
       puts_args: symbols[:puts_args].uniq,
-      tail_vars: symbols[:tail_vars].uniq
+      tail_vars: symbols[:tail_vars].uniq,
+      tail_vars_inner: symbols[:tail_vars_inner].uniq,
+      puts_inner_vars: symbols[:puts_inner_vars].uniq
     }
+  end
+
+  # putsの引数に含まれる変数を再帰抽出
+  def self.extract_puts_inner_vars(node, arr)
+    return unless node.is_a?(Array)
+    if node[0] == :command && node[1][1] == 'puts' && node[2][0] == :args_add_block
+      args = node[2][1]
+      args.each do |arg|
+        extract_inner_vars(arg, arr)
+      end
+    else
+      node.each { |child| extract_puts_inner_vars(child, arr) if child.is_a?(Array) }
+    end
+  end
+
+  # 任意の式ノードから変数名を再帰抽出
+  def self.extract_inner_vars(node, arr)
+    return unless node.is_a?(Array)
+    if node[0] == :var_ref && node[1][0] == :@ident
+      arr << node[1][1]
+    else
+      node.each { |child| extract_inner_vars(child, arr) if child.is_a?(Array) }
+    end
   end
 
   private
--- spec/lib/code_parser_spec.rb
+++ spec/lib/code_parser_spec.rb
@@ -16,6 +16,32 @@ RSpec.describe CodeParser do
     expect(symbols[:puts_args]).to include("result")
   end
 
+  it "putsの引数が複合式なら、その中の変数も除外される" do
+    code = <<~RUBY
+      a = 1
+      b = 2
+      c = 3
+      puts a + b + c
+    RUBY
+
+    symbols = CodeParser.extract_symbols(code)
+    expect(symbols[:variables]).not_to include("a", "b", "c")
+    expect(symbols[:puts_inner_vars]).to include("a", "b", "c")
+  end
+
+  it "末尾が複合式なら、その中の変数も除外される" do
+    code = <<~RUBY
+      x = 1
+      y = 2
+      z = 3
+      x + y + z
+    RUBY
+
+    symbols = CodeParser.extract_symbols(code)
+    expect(symbols[:variables]).not_to include("x", "y", "z")
+    expect(symbols[:tail_vars_inner]).to include("x", "y", "z")
+  end
+
   it "通常の代入変数はvariablesに含まれる" do
     code = <<~RUBY
       foo = 1
